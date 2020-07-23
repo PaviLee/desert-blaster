680569d3e7c272496a520bd24ca6dddf780dbdc5
Rissa Li <imgloriaborger9@gmail.com>
2020-07-23 01:07:38 -0700

    Added Part 3

---
+++ part3/README.md
@@ -0,0 +1,4 @@
+## Part 3
+
+Use KeyListener interface to make the Ship shoot a laser.   
+Make an Alien shoot a laser.
---
+++ part3/src/desertBlasters_part3_lasers/Main.java
@@ -0,0 +1,57 @@
+package desertBlasters_part3_lasers;
+
+import java.awt.Graphics;
+
+/**
+ * Run Desert Blaster from this class.
+ * 
+ * @author Rissa Li
+ * @version July 16, 2020
+ */
+public class Main {
+
+	public static int screenWidth = 800;
+	public static int screenHeight = 700;
+
+	public static void main(String[] args) {
+		Screen screen = new Screen(screenWidth, screenHeight);
+		Graphics g = screen.getScreenGraphics();
+
+		Ship ship = new Ship(screenWidth / 2, screenHeight / 2);
+		screen.addMouseMotionListener(ship);
+		screen.getJFrame().addKeyListener(ship);
+
+		Alien alien = new Alien(300, 5);
+		Astroid astroid = new Astroid(500, 5);
+
+		while (true) {
+			screen.clearScreen();
+
+			alien.move();
+			astroid.move();
+
+			alien.draw(g);
+			astroid.draw(g);
+
+			if (alien.laser != null) {
+				alien.laser.move();
+				alien.laser.draw(g);
+			}
+
+			if (ship.laser != null) {
+				ship.laser.move();
+				ship.laser.draw(g);
+			}
+
+			ship.draw(g);
+
+			screen.repaint();
+
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
